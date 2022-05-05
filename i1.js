3adb0d85d91681f247eeaaf712cd1b2a14fcb38a
nuitmod <wtvfqm@gmail.com>
2022-05-05 20:10:58 +0500

    add podcast

---
+++ i1.js
@@ -0,0 +1,6 @@
+var dat = 10;
+console.log(dat);
+function ww() {
+    console.log("fn");
+}
+ww();
---
+++ i1.ts
@@ -0,0 +1,9 @@
+var dat: number = 10;
+console.log(dat);
+
+function ww(): void{
+  console.log("fn");
+
+}
+
+ww();
